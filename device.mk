01b1d0e35f0853caff2d12a61b3cfd096330ec0a
Rajkale99 <kaleraj.rk@gmail.com>
2019-01-08 07:23:47 +0100

    rimo02a: revert "rimo02a: ril: build librmnetctl"
    This reverts commit 278f20148b903d9847e331af0e4882497b75357c.

--- device.mk
+++ device.mk
@@ -322,10 +322,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# Ril
-PRODUCT_PACKAGES += \
-    librmnetctl
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
