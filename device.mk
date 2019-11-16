TekXS/device_leeco_s2

s2: Add CarrierConfig overlays

device.mk
@@ -23,7 +23,8 @@ $(call inherit-product, vendor/leeco/s2/s2-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    vendor/aosp/overlay/CarrierConfig
 
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi

rajkale99/rimo02a-dt

Change-Id: I6782d6303a633594738b047815e9c190f2fa609c

device.mk
@@ -25,7 +25,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    vendor/arrow/overlay/CarrierConfig
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res