843c8932841c5b0a539334fdae79b37de772640f
rajkale99 <kaleraj.rk@gmail.com>
2020-07-18 18:20:12 +0000

    Revert :Rimo02a: Switch to common light hal" * Also Nule fwk-detect

--- device.mk
+++ device.mk
@@ -269,6 +269,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8952
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8952
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
